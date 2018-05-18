b150d21c10deb84a4b60cf7afa5a87a1d11932e1
Norbert Pospischek <npospischek@emarsys.com>
2018-05-18 09:20:08 +0200

    feat(magento2-api-client): introduce lib, SUITEDEV12792

--- dev/test/setup.spec.js
+++ dev/test/setup.spec.js
@@ -12,7 +12,8 @@ chai.use(chaiSubset);
 chai.use(sinonChai);
 global.expect = chai.expect;
 
-before(function() {
+before(async function() {
+  this.timeout(10000);
   this.db = knex({
     client: 'mysql',
     connection: {
@@ -22,6 +23,15 @@ before(function() {
       database: process.env.MYSQL_DATABASE
     }
   });
+
+  const result = await this.db
+    .select('value')
+    .from('core_config_data')
+    .where({ path: 'emartech/emarsys/connecttoken' })
+    .first();
+
+  const { token } = JSON.parse(Buffer.from(result.value, 'base64'));
+  this.token = token;
 });
 
 beforeEach(async function() {
