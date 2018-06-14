2e470a070114c8c293ffef69350f654165785e06
iben12 <iben@iben.hu>
2018-06-14 16:09:52 +0200

    feat(sample-data): use Magento sample data in tests MAGENTO-OPS
    Co-Authored-By: morzzz007 <morzzz007@users.noreply.github.com>

--- dev/test/products/list.spec.js
+++ dev/test/products/list.spec.js
@@ -10,24 +10,26 @@ describe('Products endpoint', function() {
     const pageSize = 10;
 
     const { products } = (await this.magentoApi.execute('products', 'get', page, pageSize))[0];
-    const product = products[1];
+    const product = products[0];
 
     expect(product.type).to.equal('simple');
     expect(product.entity_id).to.be.a('string');
     expect(product.children_entity_ids).to.be.an('array');
-    expect(product.categories[0]).to.have.members(['Simple category']);
-    expect(product.categories[1]).to.have.ordered.members(['Parent category', 'Child category']);
-    expect(product.sku).to.equal('PRODUCT-SYNC-SKU');
-    expect(product.name).to.equal('Product For Product Sync');
-    expect(product.price).to.equal('69.0000');
-    expect(product.link).to.include('http://magento.local:8888/index.php/product-for-product-sync.html');
+    expect(product.categories[0]).to.have.members(['Gear']);
+    expect(product.categories[1]).to.have.ordered.members(['Gear', 'Bags']);
+    expect(product.sku).to.equal('24-MB01');
+    expect(product.name).to.equal('Joust Duffle Bag');
+    expect(product.price).to.equal('34.0000');
+    expect(product.link).to.include('http://magento.local:8888/index.php/joust-duffle-bag.html');
     expect(product.images).to.eql({
-      image: 'http://magento.local:8888/pub/media/catalog/product',
-      small_image: 'http://magento.local:8888/pub/media/catalog/product',
-      thumbnail: 'http://magento.local:8888/pub/media/catalog/product'
+      image: 'http://magento.local:8888/pub/media/catalog/product/m/b/mb01-blue-0.jpg',
+      small_image: 'http://magento.local:8888/pub/media/catalog/product/m/b/mb01-blue-0.jpg',
+      thumbnail: 'http://magento.local:8888/pub/media/catalog/product/m/b/mb01-blue-0.jpg'
     });
-    expect(product.qty).to.equal('999.0000');
+    expect(product.qty).to.equal('100.0000');
     expect(product.is_in_stock).to.equal('1');
-    expect(product.description).to.equal('Default products description');
+    // eslint-disable-next-line
+    expect(product.description).to.equal(`<p>The sporty Joust Duffle Bag can't be beat - not in the gym, not on the luggage carousel, not anywhere. Big enough to haul a basketball or soccer ball and some sneakers with plenty of room to spare, it's ideal for athletes with places to go.<p>\n<ul>\n<li>Dual top handles.</li>\n<li>Adjustable shoulder strap.</li>\n<li>Full-length zipper.</li>\n<li>L 29\" x W 13\" x H 11\".</li>\n</ul>`
+    );
   });
 });
