7b6f4f85058d97c211bdcf825fe79f1730d66d00
iben12 <iben@iben.hu>
2018-06-15 09:43:39 +0200

    test(products-api-endpoint): add tests for paging and child entities SUITEDEV-13085

--- dev/test/products/list.spec.js
+++ dev/test/products/list.spec.js
@@ -5,31 +5,61 @@ describe('Products endpoint', function() {
 
   afterEach(async function() {});
 
-  it('returns products', async function() {
-    const page = 1;
+  it('returns products according to page and page_size', async function() {
+    const page = 3;
     const pageSize = 10;
 
     const { products } = (await this.magentoApi.execute('products', 'get', page, pageSize))[0];
     const product = products[0];
 
+    expect(products.length).to.equal(10);
+    expect(product.entity_id).to.equal('21');
+
     expect(product.type).to.equal('simple');
-    expect(product.entity_id).to.be.a('string');
     expect(product.children_entity_ids).to.be.an('array');
     expect(product.categories[0]).to.have.members(['Gear']);
-    expect(product.categories[1]).to.have.ordered.members(['Gear', 'Bags']);
-    expect(product.sku).to.equal('24-MB01');
-    expect(product.name).to.equal('Joust Duffle Bag');
-    expect(product.price).to.equal('34.0000');
-    expect(product.link).to.include('http://magento.local:8888/index.php/joust-duffle-bag.html');
+    expect(product.categories[1]).to.have.ordered.members(['Gear', 'Fitness Equipment']);
+    expect(product.sku).to.equal('24-WG084');
+    expect(product.name).to.equal('Sprite Foam Yoga Brick');
+    expect(product.price).to.equal('5.0000');
+    expect(product.link).to.include('http://magento.local:8888/index.php/sprite-foam-yoga-brick.html');
     expect(product.images).to.eql({
-      image: 'http://magento.local:8888/pub/media/catalog/product/m/b/mb01-blue-0.jpg',
-      small_image: 'http://magento.local:8888/pub/media/catalog/product/m/b/mb01-blue-0.jpg',
-      thumbnail: 'http://magento.local:8888/pub/media/catalog/product/m/b/mb01-blue-0.jpg'
+      image: 'http://magento.local:8888/pub/media/catalog/product/l/u/luma-yoga-brick.jpg',
+      small_image: 'http://magento.local:8888/pub/media/catalog/product/l/u/luma-yoga-brick.jpg',
+      thumbnail: 'http://magento.local:8888/pub/media/catalog/product/l/u/luma-yoga-brick.jpg'
     });
     expect(product.qty).to.equal('100.0000');
     expect(product.is_in_stock).to.equal('1');
-    // eslint-disable-next-line
-    expect(product.description).to.equal(`<p>The sporty Joust Duffle Bag can't be beat - not in the gym, not on the luggage carousel, not anywhere. Big enough to haul a basketball or soccer ball and some sneakers with plenty of room to spare, it's ideal for athletes with places to go.<p>\n<ul>\n<li>Dual top handles.</li>\n<li>Adjustable shoulder strap.</li>\n<li>Full-length zipper.</li>\n<li>L 29\" x W 13\" x H 11\".</li>\n</ul>`
+    expect(product.description).to.equal(
+      // eslint-disable-next-line
+      `<p>Our top-selling yoga prop, the 4-inch, high-quality Sprite Foam Yoga Brick is popular among yoga novices and studio professionals alike. An essential yoga accessory, the yoga brick is a critical tool for finding balance and alignment in many common yoga poses. Choose from 5 color options.</p>\n<ul>\n<li>Standard Large Size: 4\" x 6\" x 9\".\n<li>Beveled edges for ideal contour grip.\n<li>Durable and soft, scratch-proof foam.\n<li>Individually wrapped.\n<li>Ten color choices.\n</ul> `
     );
+  });
+
+  it('returns child entities for configurable products', async function() {
+    const page = 67;
+    const pageSize = 1;
+
+    const { products } = (await this.magentoApi.execute('products', 'get', page, pageSize))[0];
+    const product = products[0];
+
+    expect(product.type).to.equal('configurable');
+    expect(product.children_entity_ids).to.eql([
+      '52',
+      '53',
+      '54',
+      '55',
+      '56',
+      '57',
+      '58',
+      '59',
+      '60',
+      '61',
+      '62',
+      '63',
+      '64',
+      '65',
+      '66'
+    ]);
   });
 });
