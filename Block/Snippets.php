c7fcfc5da63889739bd157ea4a96167500d3cfdb
Norbert Pospischek <npospischek@emarsys.com>
2018-06-26 11:26:26 +0200

    feat(webextend): show customer id on every page SUITEDEV-13242
    Co-Authored-By: boogie <boogie@users.noreply.github.com>

--- Block/Snippets.php
+++ Block/Snippets.php
@@ -5,7 +5,10 @@
  * @copyright  Copyright (c) 2018 Emarsys. (http://www.emarsys.net/)
  */
 namespace Emartech\Emarsys\Block;
+
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Customer\Model\Session as CustomerSession;
+
 /**
  * Class Snippets
  * @package Emartech\Emarsys\Block
@@ -13,15 +16,42 @@ use Magento\Framework\View\Element\Template\Context;
 class Snippets extends \Magento\Framework\View\Element\Template
 {
     /**
-     * JavascriptTracking constructor.
+     * @var CustomerSession
+     */
+    protected $customerSession;
+
+    /**
+     * Snippets constructor.
      *
      * @param Context $context
+     * @param CustomerSession $customerSession
      * @param array $data
      */
     public function __construct(
       Context $context,
+      CustomerSession $customerSession,
       array $data = []
     ) {
+      $this->customerSession = $customerSession;
       parent::__construct($context, $data);
+    }
+
+    /**
+     * Get Customer Id
+     *
+     * @return bool|string
+     */
+    public function getCustomerId()
+    {
+      try {
+        if ($this->customerSession->isLoggedIn()) {
+          $customer = $this->customerSession->getCustomer();
+          return $customer->getEntityId();
+        }
+      } catch (\Exception $e) {
+          throw $e;
+      }
+
+      return false;
     }
 }
