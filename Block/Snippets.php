83d336ddb08e066a163879e3a9e8f5bff527b6bc
Norbert Pospischek <npospischek@emarsys.com>
2018-06-27 13:44:38 +0200

    feat(frontend): addsku, catalog, search term to page SUITDEV-13244
    Co-Authored-By: morzzz007 <morzzz007@users.noreply.github.com>

--- Block/Snippets.php
+++ Block/Snippets.php
@@ -7,7 +7,9 @@
 namespace Emartech\Emarsys\Block;
 
 use Magento\Framework\View\Element\Template\Context;
-use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\Registry;
 
 /**
  * Class Snippets
@@ -15,43 +17,104 @@ use Magento\Customer\Model\Session as CustomerSession;
  */
 class Snippets extends \Magento\Framework\View\Element\Template
 {
-    /**
-     * @var CustomerSession
+  /**
+     * @var \Magento\Store\Model\StoreManagerInterface
      */
-    protected $customerSession;
+    protected $storeManager;
 
-    /**
-     * Snippets constructor.
-     *
-     * @param Context $context
-     * @param CustomerSession $customerSession
-     * @param array $data
-     */
-    public function __construct(
-      Context $context,
-      CustomerSession $customerSession,
-      array $data = []
-    ) {
-      $this->customerSession = $customerSession;
-      parent::__construct($context, $data);
-    }
+  /**
+   * @var CategoryFactory
+   */
+  protected $categoryFactory;
 
-    /**
-     * Get Customer Id
-     *
-     * @return bool|string
-     */
-    public function getCustomerId()
-    {
-      try {
-        if ($this->customerSession->isLoggedIn()) {
-          $customer = $this->customerSession->getCustomer();
-          return $customer->getEntityId();
-        }
-      } catch (\Exception $e) {
-          throw $e;
+  /**
+   * @var Registry
+   */
+  protected $coreRegistry;
+
+  /**
+   * Snippets constructor.
+   *
+   * @param Context $context
+   * @param CategoryFactory $categoryFactory
+   * @param Http $request
+   * @param Registry $registry
+   * @param array $data
+   */
+  public function __construct(
+    Context $context,
+    CategoryFactory $categoryFactory,
+    Http $request,
+    Registry $registry,
+    array $data = []
+  ) {
+    $this->storeManager = $context->getStoreManager();
+    $this->categoryFactory = $categoryFactory;
+    $this->_request = $request;
+    $this->coreRegistry = $registry;
+    parent::__construct($context, $data);
+  }
+
+  /**
+   * Get Sku
+   *
+   * @return string
+   */
+  public function getSku()
+  {
+    try {
+      $product = $this->coreRegistry->registry('current_product');
+      if (isset($product) && $product != '') {
+        return addslashes($product->getSku());
       }
-
-      return false;
+    } catch (\Exception $e) {
+      throw $e;
     }
+
+    return false;
+  }
+
+  /**
+   * Get Search Term
+   *
+   * @return bool|mixed
+   */
+  public function getSearchTerm()
+  {
+    try {
+      $q = $this->_request->getParam('q');
+      if ($q != '') {
+        return $q;
+      }
+    } catch (\Exception $e) {
+      throw $e;
+    }
+    return false;
+  }
+
+  /**
+   * Get Category
+   *
+   * @return string
+   */
+  public function getCategory()
+  {
+    try {
+      $category = $this->coreRegistry->registry('current_category');
+      if (isset($category) && $category != '') {
+        $categoryPath = $category->getPath();
+        $categoryIds = explode('/', $categoryPath);
+        $categoryList = [];
+        for ($pathIndex = 2; $pathIndex < count($categoryIds); $pathIndex++) {
+          $storeId = $this->storeManager->getDefaultStoreView()->getId();
+          $childCat = $this->categoryFactory->create()->setStoreId($storeId)->load($categoryIds[$pathIndex]);
+          $categoryList[] = $childCat->getName();
+        }
+        return implode(" > ", $categoryList);
+      }
+    } catch (\Exception $e) {
+      throw $e;
+    }
+    return false;
+  }
 }
