0b8584025d62ccbcb1514ba36f97983cedfb88f2
Marton Papp <marton.papp@emarsys.com>
2018-07-18 10:48:51 +0200

    feat(web-extend): add missing test SUITEDEV-13572

---
+++ dev/test/webextend/script.spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Magento2ApiClient = require('@emartech/magento2-api');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+let magentoApi;
+
+describe('Webextend scripts', function() {
+  before(function() {
+    magentoApi = new Magento2ApiClient({
+      baseUrl: 'http://web',
+      token: this.token
+    });
+  });
+
+  it('should be in the HTML if injectsnippet is enabled', async function() {
+    await magentoApi.setSettings({
+      injectSnippet: 'enabled',
+      merchantId: '123',
+      webTrackingSnippetUrl: 'http://yolo.hu/script'
+    });
+
+    const response = await axios.get('http://magento.local/index.php');
+    const $ = cheerio.load(response.data);
+    const emarsysSnippets = $('.emarsys-snippets').html().replace(/(?:\r\n|\r|\n)/g, '');
+
+    expect(emarsysSnippets.includes('<script src="http://yolo.hu/script"></script>')).to.be.true;
+
+    //eslint-disable-next-line
+    expect(emarsysSnippets.includes(`<script type="text/javascript">    var ScarabQueue = ScarabQueue || [];    (function(id) {      if (document.getElementById(id)) return;      var js = document.createElement('script'); js.id = id;      js.src = '//cdn.scarabresearch.com/js/123/scarab-v2.js';      var fs = document.getElementsByTagName('script')[0];      fs.parentNode.insertBefore(js, fs);    })('scarab-js-api');  </script>`)).to.be.true;
+
+    //eslint-disable-next-line
+    expect(emarsysSnippets.includes('<script>Emarsys.Magento2.track({"product":false,"category":false,"store":{"merchantId":"123"},"search":false});</script>')).to.be.true;
+  });
+});
