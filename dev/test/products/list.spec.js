ffdaff6940085e931fc81c5725dc25a77e928437
Norbert Pospischek <npospischek@emarsys.com>
2018-09-14 15:20:12 +0200

    test(product-api): fix tests SUITEDEV-14346

--- dev/test/products/list.spec.js
+++ dev/test/products/list.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe.skip('Products endpoint', function() {
+describe('Products endpoint', function() {
   before(function() {});
 
   afterEach(async function() {});
@@ -13,26 +13,28 @@ describe.skip('Products endpoint', function() {
     const product = products[0];
 
     expect(products.length).to.equal(10);
-    expect(product.entity_id).to.equal('21');
     expect(productCount).to.equal(2048);
 
+    expect(product.entity_id).to.equal(21);
     expect(product.type).to.equal('simple');
     expect(product.children_entity_ids).to.be.an('array');
-    expect(product.categories[0]).to.have.members(['Gear']);
-    expect(product.categories[1]).to.have.ordered.members(['Gear', 'Fitness Equipment']);
+    expect(product.categories[0]).to.be.equal('1/2/3');
+    expect(product.categories[1]).to.be.equal('1/2/3/5');
     expect(product.sku).to.equal('24-WG084');
-    expect(product.name).to.equal('Sprite Foam Yoga Brick');
-    expect(product.price).to.equal('5.0000');
-    expect(product.link).to.include('/index.php/sprite-foam-yoga-brick.html');
+    expect(product.qty).to.equal(100);
+    expect(product.is_in_stock).to.equal(1);
     expect(product.images).to.eql({
       image: `http://${this.hostname}/pub/media/catalog/product/l/u/luma-yoga-brick.jpg`,
       small_image: `http://${this.hostname}/pub/media/catalog/product/l/u/luma-yoga-brick.jpg`,
       thumbnail: `http://${this.hostname}/pub/media/catalog/product/l/u/luma-yoga-brick.jpg`
     });
-    expect(product.qty).to.equal('100.0000');
-    expect(product.is_in_stock).to.equal('1');
-    expect(product.status).to.equal('1');
-    expect(product.description).to.equal(
+
+    const storeLevelProduct = product.store_data[0];
+    expect(storeLevelProduct.name).to.equal('Sprite Foam Yoga Brick');
+    expect(storeLevelProduct.price).to.equal(5);
+    expect(storeLevelProduct.link).to.include('/index.php/sprite-foam-yoga-brick.html');
+    expect(storeLevelProduct.status).to.equal(1);
+    expect(storeLevelProduct.description).to.equal(
       // eslint-disable-next-line
       `<p>Our top-selling yoga prop, the 4-inch, high-quality Sprite Foam Yoga Brick is popular among yoga novices and studio professionals alike. An essential yoga accessory, the yoga brick is a critical tool for finding balance and alignment in many common yoga poses. Choose from 5 color options.</p>\n<ul>\n<li>Standard Large Size: 4\" x 6\" x 9\".\n<li>Beveled edges for ideal contour grip.\n<li>Durable and soft, scratch-proof foam.\n<li>Individually wrapped.\n<li>Ten color choices.\n</ul> `
     );
@@ -47,21 +49,21 @@ describe.skip('Products endpoint', function() {
 
     expect(product.type).to.equal('configurable');
     expect(product.children_entity_ids).to.eql([
-      '52',
-      '53',
-      '54',
-      '55',
-      '56',
-      '57',
-      '58',
-      '59',
-      '60',
-      '61',
-      '62',
-      '63',
-      '64',
-      '65',
-      '66'
+      52,
+      53,
+      54,
+      55,
+      56,
+      57,
+      58,
+      59,
+      60,
+      61,
+      62,
+      63,
+      64,
+      65,
+      66
     ]);
   });
 });
