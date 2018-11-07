d206d41e8e7fcb9243e7f9a408494256d23cf068
Roman Stepanov <mail2punk@gmail.com>
2018-11-07 15:41:47 +0200

    reverted debug

--- registration.php
+++ registration.php
@@ -3,5 +3,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Emartech_Emarsys',
-    isset($file) ? dirname($file) : __DIR__
+    __DIR__
 );
