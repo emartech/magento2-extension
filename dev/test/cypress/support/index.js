ccb54fbe805b9b7cb956240d6a817a750a77fe13
Norbert Pospischek <npospischek@emarsys.com>
2018-12-20 13:21:51 +0100

    test(e2e): catch aborted xhr request error from magento MAGENTO-OPS

--- dev/test/cypress/support/index.js
+++ dev/test/cypress/support/index.js
@@ -21,11 +21,33 @@ before(()=> {
 });
 
 afterEach(() => {
-  cy.wait(4000);
   cy.task('clearEvents');
 });
 
 Cypress.on('uncaught:exception', (err, runnable) => { // eslint-disable-line no-unused-vars
   cy.task('log', err.toString());
   return false;
+});
+
+const erroredXHRRequest = test => {
+  return test.commands.filter(command => {
+    if (command.name === 'xhr') {
+      return command.snapshots.some(snapshot => snapshot.name === 'error');
+    }
+  }).length;
+};
+
+const customerDataInStack = err => err.stack.indexOf('Magento/luma/en_US/Magento_Customer/js/customer-data.js') > -1;
+
+const invalidFail = (err, test) => {
+  const hasCustomDataJsInStack = customerDataInStack(err);
+  const hasErroredXhrRequest = erroredXHRRequest(test);
+  return hasCustomDataJsInStack && hasErroredXhrRequest;
+};
+
+Cypress.on('fail', (err, runnable) => {
+  if (invalidFail(err, runnable)) {
+    return true;
+  }
+  throw err;
 });
