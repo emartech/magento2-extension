19f617e1473d19fa5e02bcb0cd15c594c0ab5f3e
Norbert Pospischek <npospischek@emarsys.com>
2018-12-21 15:33:33 +0100

    revert(e2e): revert catching magento xhr fails MAGENTO-OPS

--- dev/test/cypress/support/index.js
+++ dev/test/cypress/support/index.js
@@ -30,26 +30,26 @@ Cypress.on('uncaught:exception', (err, runnable) => { // eslint-disable-line no-
   return false;
 });
 
-const erroredXHRRequest = test => {
-  return test.commands.filter(command => {
-    if (command.name === 'xhr') {
-      return command.snapshots.some(snapshot => snapshot.name === 'error');
-    }
-  }).length;
-};
+// const erroredXHRRequest = test => {
+//   return test.commands.filter(command => {
+//     if (command.name === 'xhr') {
+//       return command.snapshots.some(snapshot => snapshot.name === 'error');
+//     }
+//   }).length;
+// };
 
-const customerDataInStack = err => err.stack.indexOf('Magento/luma/en_US/Magento_Customer/js/customer-data.js') > -1;
+// const customerDataInStack = err => err.stack.indexOf('Magento/luma/en_US/Magento_Customer/js/customer-data.js') > -1;
 
-const invalidFail = (err, test) => {
-  const hasCustomDataJsInStack = customerDataInStack(err);
-  const hasErroredXhrRequest = erroredXHRRequest(test);
-  return hasCustomDataJsInStack && hasErroredXhrRequest;
-};
+// const invalidFail = (err, test) => {
+//   const hasCustomDataJsInStack = customerDataInStack(err);
+//   const hasErroredXhrRequest = erroredXHRRequest(test);
+//   return hasCustomDataJsInStack && hasErroredXhrRequest;
+// };
 
-Cypress.on('fail', (err, runnable) => {
-  if (invalidFail(err, runnable)) {
-    console.log('Errored because of failed XHR abort. Please check this fail.');
-    return true;
-  }
-  throw err;
-});
+// Cypress.on('fail', (err, runnable) => {
+//   if (invalidFail(err, runnable)) {
+//     console.log('Errored because of failed XHR abort. Please check this fail.');
+//     return true;
+//   }
+//   throw err;
+// });
