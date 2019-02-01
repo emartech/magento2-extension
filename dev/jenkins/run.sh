104527858daf20d485d1814fdf48d31f48b2c5fc
iben12 <iben@iben.hu>
2019-02-01 11:03:47 +0100

    test(ci): create run script fo Jenkins CDPIP-1039

---
+++ dev/jenkins/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+set -e
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+docker-compose down
+
+echo "\n|--- Starting containers"
+docker-compose up -d
+echo "\n|--- Waiting for containers to initialize"
+sh ./wait.sh
+echo "\n|--- Checking Magento code style"
+docker-compose exec -T magento-test bash -c "sh vendor/emartech/emarsys-magento2-extension/dev/codesniffer.sh"
+echo "\n|--- Testing Magento DI compilation"
+docker-compose exec -T --user application magento-test bash -c "bin/magento setup:di:compile"
+echo "\n\n|--- Restarting Magento container"
+docker-compose stop magento-test
+docker-compose rm -f magento-test
+docker-compose up -d
+echo "\n|--- Waiting for Magento init"
+sh ./wait.sh
+echo "\n|--- Running backend tests"
+docker-compose run --rm node sh -c "npm i && npm t"
+echo "\n\n|--- Restarting containers"
+docker-compose down
+docker-compose up -d
+echo "\n|--- Waiting for containers to initialize"
+sh ./wait.sh
+echo "\n|--- Running frontend tests"
+docker-compose run --rm node sh -c "npm i && npm run e2e"
+
+echo "\n\n|--- All tests passed"
---
+++ dev/jenkins/wait.sh
@@ -0,0 +1,25 @@
+#$/usr/bin/env bash
+
+wait_it()
+{
+  while :
+  do
+    result=$(docker-compose ps | grep magento)
+
+    if [[ ! -z "$result" ]]; then
+      healthy=$(echo $result | grep "UP (healthy)")
+      if [[ ! -z "$healthy" ]]; then
+        echo "ready"
+        break
+      fi
+      exited=$(echo $result | grep "Exit")
+      if [[ ! -z "$exited" ]]; then
+        echo "exited"
+        exit 1
+      fi
+    fi
+    sleep 1
+  done
+}
+
+wait_it
