63ac8a54f36c3038a632ef0e1f0ef6cb75a8855c
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2019-02-04 14:43:04 +0100

    test(ci): Fix Docker Compose build command CDPIP-1036

--- dev/jenkins/run.sh
+++ dev/jenkins/run.sh
@@ -9,7 +9,7 @@ alias compose="docker-compose -p mage_$VERSION"
 
 compose down
 
-compose build node --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy
+compose build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy node
 
 echo "\n|--- Running tests on Magento $VERSION"
 echo "\n|--- Starting containers"
