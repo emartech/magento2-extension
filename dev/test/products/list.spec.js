aeeda69bb61322475e0ef07abacb14150fc73b46
Marton Papp <marton.papp@emarsys.com>
2019-02-06 13:05:06 +0100

    feat(fixtures): create product fixture CDPIP-1042

--- dev/test/products/list.spec.js
+++ dev/test/products/list.spec.js
@@ -1,43 +1,33 @@
 'use strict';
 
+const { getProducts } = require('../fixtures/products');
+
 describe('Products endpoint', function() {
-  before(function() {});
-
-  afterEach(async function() {});
-
   it('returns product count and products according to page and page_size', async function() {
     const page = 3;
     const limit = 10;
 
     const { products, productCount } = await this.magentoApi.execute('products', 'get', { page, limit });
+    const { products: expectedProducts, productCount: expectedProductCount } = getProducts(this.hostname, page, limit);
+
     const product = products[0];
+    const expectedProduct = expectedProducts[0];
 
-    expect(products.length).to.equal(10);
-    expect(productCount).to.equal(2048);
+    expect(products.length).to.equal(expectedProducts.length);
+    expect(productCount).to.equal(expectedProductCount);
 
-    expect(product.entity_id).to.equal(21);
-    expect(product.type).to.equal('simple');
-    expect(product.children_entity_ids).to.be.an('array');
-    expect(product.categories[0]).to.be.equal('1/2/3');
-    expect(product.categories[1]).to.be.equal('1/2/3/5');
-    expect(product.sku).to.equal('24-WG084');
-    expect(product.qty).to.equal(100);
-    expect(product.is_in_stock).to.equal(1);
-    expect(product.images).to.eql({
-      image: `http://${this.hostname}/pub/media/catalog/product/l/u/luma-yoga-brick.jpg`,
-      small_image: `http://${this.hostname}/pub/media/catalog/product/l/u/luma-yoga-brick.jpg`,
-      thumbnail: `http://${this.hostname}/pub/media/catalog/product/l/u/luma-yoga-brick.jpg`
+    ['entity_id', 'type', 'sku', 'qty', 'is_in_stock', 'images'].forEach(key => {
+      expect(product[key]).to.eql(expectedProduct[key]);
     });
 
+    expect(product.children_entity_ids).to.be.an('array');
+    expect(product.categories[0]).to.equal(expectedProduct.categories[0]);
+    expect(product.categories[1]).to.equal(expectedProduct.categories[1]);
+
     const storeLevelProduct = product.store_data[0];
-    expect(storeLevelProduct.name).to.equal('Sprite Foam Yoga Brick');
-    expect(storeLevelProduct.price).to.equal(5);
-    expect(storeLevelProduct.link).to.include('/index.php/sprite-foam-yoga-brick.html');
-    expect(storeLevelProduct.status).to.equal(1);
-    expect(storeLevelProduct.description).to.equal(
-      // eslint-disable-next-line
-      `<p>Our top-selling yoga prop, the 4-inch, high-quality Sprite Foam Yoga Brick is popular among yoga novices and studio professionals alike. An essential yoga accessory, the yoga brick is a critical tool for finding balance and alignment in many common yoga poses. Choose from 5 color options.</p>\n<ul>\n<li>Standard Large Size: 4\" x 6\" x 9\".\n<li>Beveled edges for ideal contour grip.\n<li>Durable and soft, scratch-proof foam.\n<li>Individually wrapped.\n<li>Ten color choices.\n</ul> `
-    );
+    ['name', 'price', 'link', 'status', 'description'].forEach(key => {
+      expect(storeLevelProduct[key]).to.equal(expectedProduct.store_data[0][key]);
+    });
   });
 
   it('returns child entities for configurable products', async function() {
@@ -45,25 +35,12 @@ describe('Products endpoint', function() {
     const limit = 1;
 
     const { products } = await this.magentoApi.execute('products', 'get', { page, limit });
+    const { products: expectedProducts } = getProducts(this.hostname, page, limit);
     const product = products[0];
+    const expectedProduct = expectedProducts[0];
 
-    expect(product.type).to.equal('configurable');
-    expect(product.children_entity_ids).to.eql([
-      52,
-      53,
-      54,
-      55,
-      56,
-      57,
-      58,
-      59,
-      60,
-      61,
-      62,
-      63,
-      64,
-      65,
-      66
-    ]);
+    ['type', 'children_entity_ids'].forEach(key => {
+      expect(product[key]).to.eql(expectedProduct[key]);
+    });
   });
 });
