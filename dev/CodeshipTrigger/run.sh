633b4af94a2ef8a767570105495ca19dd7ea99b2
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2019-03-08 11:34:02 +0100

    test(ci): Pass http and https proxies as env vars so axios can use them CDPIP-CHORE

--- dev/CodeshipTrigger/run.sh
+++ dev/CodeshipTrigger/run.sh
@@ -14,6 +14,8 @@ docker run --rm \
     -e "CODESHIP_USER=$CODESHIP_USER" \
     -e "CODESHIP_PASSWORD=$CODESHIP_PASSWORD" \
     -e "REVISION=$GIT_COMMIT" \
+    -e "http_proxy=$http_proxy" \
+    -e "https_proxy=$https_proxy" \
     node bash -c "npm config set proxy $http_proxy && npm config set https-proxy $https_proxy && npm i && node index.js" \
     --http-proxy=$http_proxy \
     --https-proxy=$https_proxy
