6e3167fcc24894e3caa45cd2567cadd8d0005e17
Norbert Pospischek <npospischek@emarsys.com>
2019-05-03 15:51:35 +0200

    test(e2e): add logging to CI and CyDash CDPIP-OPS

--- dev/test/cypress/support/index.js
+++ dev/test/cypress/support/index.js
@@ -12,3 +12,39 @@ Cypress.on('uncaught:exception', (err, runnable) => { // eslint-disable-line no-
   console.log('uncaught:exception', err.toString());
   return false;
 });
+
+let logs = '';
+
+Cypress.on('window:before:load', window => {
+  const docIframe = window.parent.document.getElementById("Your App: 'test'");
+  const appWindow = docIframe.contentWindow;
+
+  ['log', 'info', 'error', 'warn', 'debug'].forEach(consoleProperty => {
+    appWindow.console[consoleProperty] = function(...args) {
+      logs += args.join(' ') + '\n';
+    };
+  });
+});
+
+Cypress.mocha.getRunner().on('test', () => {
+  logs = '';
+});
+
+Cypress.on('fail', error => {
+  if (!error) {
+    error = '';
+  }
+  error.stack += '\nConsole Logs:\n========================\n';
+  error.stack += logs;
+  logs = '';
+  throw error;
+});
+
+console.log = function(...args) {
+  logs += args.join('\n');
+};
+
+Cypress.Commands.overwrite('log', (originalFn, ...args) => {
+  logs += args.join('\n');
+  originalFn(...args);
+});
