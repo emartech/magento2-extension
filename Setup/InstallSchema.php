1c802ea18c2cfc340d35f7ee1482a8244ddec9e7
iben12 <iben@iben.hu>
2019-06-05 13:12:19 +0200

    fix(schema): fix extension schema install script CDPIP-1430

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -77,7 +77,7 @@ class InstallSchema implements InstallSchemaInterface
                 )
                 ->addColumn(
                     'event_data',
-                    'mediumblob',
+                    \Magento\Framework\DB\Ddl\Table::TYPE_BLOB,
                     null,
                     ['default' => null, 'nullable' => false],
                     'Event Data'
@@ -108,6 +108,7 @@ class InstallSchema implements InstallSchemaInterface
                     ['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_INDEX]
                 );
             $setup->getConnection()->createTable($table);
+            $setup->getConnection()->modifyColumn($tableName, 'event_data', 'mediumblob');
         }
     }
 }
