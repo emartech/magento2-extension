03ceb033b6d420b37a04c89f190f958e4526a1cd
Kapu Erik <erko256@gmail.com>
2019-11-14 12:31:35 +0100

    test(products-endpoint): add extra fields test CDPIP-2093
    Co-Authored-By: BlasiusVonSzerencsi <BlasiusVonSzerencsi@users.noreply.github.com>

--- dev/test/products/list.spec.js
+++ dev/test/products/list.spec.js
@@ -1,8 +1,41 @@
 'use strict';
 
+const axios = require('axios');
+
 const { getProducts } = require('../fixtures/products');
 
+const getAxiosInstance = (hostname, token) =>
+  axios.create({
+    baseURL: `http://${hostname}`,
+    headers: { Authorization: `Bearer ${token}` },
+    timeout: 30000
+  });
+
+const getFirstProduct = async (hostname, token) => {
+  const response = await getAxiosInstance(hostname, token).get(
+    'index.php/rest/V1/emarsys/products?page=1&page_size=1&store_id=0,1'
+  );
+  return response.data.products[0];
+};
+
 describe('Products endpoint', function() {
+
+  before(async function() {
+    await this.magentoApi.execute('attributes', 'set', {
+      websiteId: 0,
+      type: 'product',
+      attributeCodes: ['emarsys_test_fuel_type']
+    });
+  });
+
+  after(async function() {
+    await this.magentoApi.execute('attributes', 'set', {
+      websiteId: 0,
+      type: 'product',
+      attributeCodes: []
+    });
+  });
+
   it('returns product count and products according to page and page_size', async function() {
     const page = 3;
     const limit = 10;
@@ -80,5 +113,31 @@ describe('Products endpoint', function() {
     ['type', 'children_entity_ids'].forEach(key => {
       expect(product[key]).to.eql(expectedProduct[key]);
     });
+  });
+
+  it('returns extra_fields for products', async function() {
+    const product = await getFirstProduct(this.hostname, this.token);
+
+    const path = `index.php/rest/V1/products/${product.sku}`;
+    const payload = {
+      product: {
+        custom_attributes: [
+          {
+            attribute_code: 'emarsys_test_fuel_type',
+            value: 'gasoline'
+          },
+          {
+            attribute_code: 'emarsys_test_number_of_seats',
+            value: 6
+          }
+        ]
+      }
+    };
+    await getAxiosInstance(this.hostname, this.token).put(path, payload);
+    const updatedProduct = await getFirstProduct(this.hostname, this.token);
+
+    expect(updatedProduct.store_data[0].extra_fields[0].key).to.be.equal('emarsys_test_fuel_type');
+    expect(updatedProduct.store_data[0].extra_fields[0].value).to.be.equal('gasoline');
+    expect(updatedProduct.store_data[0].extra_fields.length).to.be.equal(1);
   });
 });
