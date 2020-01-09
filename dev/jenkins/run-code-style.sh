da3214f175d11a9b0ab3bae9c13bed4937cde8d5
iben12 <iben@iben.hu>
2020-01-09 22:14:56 +0100

    ops(ci): simplify Magento code-style step CDPIP-OPS

--- dev/jenkins/run-code-style.sh
+++ dev/jenkins/run-code-style.sh
@@ -9,19 +9,7 @@ alias compose="docker-compose -p mage_$VERSION"
 
 compose down
 
-if [[ "$TABLE_PREFIX" == "" ]]
-then
-  echo "\n|--- Running tests on Magento $VERSION"
-else
-  echo "\n|--- Running tests on Magento $VERSION with $TABLE_PREFIX"
-fi
-echo "\n|--- Starting containers"
-compose up -d
-echo "\n|--- Waiting for containers to initialize"
-sh ./wait.sh >>/dev/null 2>&1
 echo "\n|--- Checking Magento code style"
-compose exec -T magento-test bash -c "sh vendor/emartech/emarsys-magento2-extension/dev/codesniffer.sh"
-
-echo "\n\n|--- All tests passed"
+compose run -T magento-test bash -c "sh vendor/emartech/emarsys-magento2-extension/dev/codesniffer.sh"
 
 compose down
