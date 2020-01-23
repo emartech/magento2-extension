5aee6e32cbc474884315f7a161fdf20b3531522d
iben12 <iben@iben.hu>
2020-01-23 14:50:39 +0100

    ci(code-style): fix code style runner script CDPIP-OPS

--- dev/jenkins/run-code-style.sh
+++ dev/jenkins/run-code-style.sh
@@ -6,4 +6,6 @@ BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
 docker run --rm \
--v $(pwd)/../../:/app/vendor/emartech/emarsys-magento2-extension emarsys/ems-integration-magento-sampledata:$VERSION bash -c "sh /app/vendor/emartech/emarsys-magento2-extension/dev/codesniffer.sh"
+-v "$(pwd)/../../:/app/vendor/emartech/emarsys-magento2-extension" \
+emarsys/ems-integration-magento-sampledata:$VERSION \
+bash -c "sh /app/vendor/emartech/emarsys-magento2-extension/dev/codesniffer.sh"
