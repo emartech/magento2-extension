e6fbe20e7c33a54b9b3ca3a4ca0f7af6db1aa53b
Marton Papp <morzzz007@gmail.com>
2020-03-05 13:17:31 +0100

    fix(orders-test): create and ship test orders one-by-one CDPIP-OPS
    Co-Authored-By: BlasiusVonSzerencsi <BlasiusVonSzerencsi@users.noreply.github.com>

--- dev/test/orders/get.spec.js
+++ dev/test/orders/get.spec.js
@@ -1,69 +1,6 @@
 'use strict';
 
-const localAddresses = {
-  shippingAddress: {
-    region: 'New York',
-    region_id: 43,
-    region_code: 'NY',
-    country_id: 'US',
-    street: ['123 Oak Ave'],
-    postcode: '10577',
-    city: 'Purchase',
-    firstname: 'Jane',
-    lastname: 'Doe',
-    email: 'jdoe@example.shipping.com',
-    telephone: '512-555-1111'
-  },
-  billingAddress: {
-    region: 'New York',
-    region_id: 43,
-    region_code: 'NY',
-    country_id: 'US',
-    street: ['123 Oak Ave'],
-    postcode: '10577',
-    city: 'Purchase',
-    firstname: 'Jane',
-    lastname: 'Doe',
-    email: 'jdoe@example.billing.com',
-    telephone: '512-555-1111'
-  },
-  shipping_carrier_code: 'flatrate',
-  shipping_method_code: 'flatrate'
-};
-
-const createNewCustomerOrder = async (magentoApi, localCartItem) => {
-  const { data: cartId } = await magentoApi.post({
-    path: '/index.php/rest/V1/guest-carts'
-  });
-  await magentoApi.post({
-    path: `/index.php/rest/V1/guest-carts/${cartId}/items`,
-    payload: {
-      cartItem: { ...localCartItem, quote_id: cartId }
-    }
-  });
-  await magentoApi.post({
-    path: `/index.php/rest/V1/guest-carts/${cartId}/shipping-information`,
-    payload: {
-      addressInformation: localAddresses
-    }
-  });
-
-  try {
-    const { data: orderId } = await magentoApi.put({
-      path: `/index.php/rest/V1/guest-carts/${cartId}/order`,
-      payload: {
-        paymentMethod: {
-          method: 'checkmo'
-        }
-      }
-    });
-
-    return { cartId, orderId };
-  } catch (error) {
-    const util = require('util');
-    console.log(`Error during completing ${cartId}, ${error.message}, ${util.inspect(error.response)}`);
-  }
-};
+const { shipOrder, createNewGuestOrder } = require('../helpers/orders');
 
 const orderCount = 4;
 
@@ -73,7 +10,11 @@ describe('Orders endpoint', function() {
   before(async function() {
     await this.dbCleaner.clearOrders();
     localCartItem = this.localCartItem;
-    await Promise.all([...Array(orderCount)].map(() => createNewCustomerOrder(this.magentoApi, localCartItem)));
+
+    for (let index = 0; index < orderCount; index++) {
+      const { orderId } = await createNewGuestOrder(this.magentoApi, localCartItem);
+      await shipOrder(this.magentoApi, orderId);
+    }
   });
 
   after(async function() {
