04e891cfedcc6a7b36a9023813ccf782082f30cb
Norbert Pospischek <npospischek@emarsys.com>
2020-03-05 10:34:21 +0100

    test(products-delta): add test for stock change through order  CDPIP-2361
    Co-Authored-By: paljenczy <paljenczy@users.noreply.github.com>

--- dev/test/products/deltas-stock-change.spec.js
+++ dev/test/products/deltas-stock-change.spec.js
@@ -1,5 +1,77 @@
 'use strict';
 
+const localAddresses = {
+  shippingAddress: {
+    region: 'New York',
+    region_id: 43,
+    region_code: 'NY',
+    country_id: 'US',
+    street: ['123 Oak Ave'],
+    postcode: '10577',
+    city: 'Purchase',
+    firstname: 'Jane',
+    lastname: 'Doe',
+    email: 'jdoe@example.shipping.com',
+    telephone: '512-555-1111'
+  },
+  billingAddress: {
+    region: 'New York',
+    region_id: 43,
+    region_code: 'NY',
+    country_id: 'US',
+    street: ['123 Oak Ave'],
+    postcode: '10577',
+    city: 'Purchase',
+    firstname: 'Jane',
+    lastname: 'Doe',
+    email: 'jdoe@example.billing.com',
+    telephone: '512-555-1111'
+  },
+  shipping_carrier_code: 'flatrate',
+  shipping_method_code: 'flatrate'
+};
+
+const createNewCustomerOrderAndShip = async (magentoApi, localCartItem) => {
+  const { data: cartId } = await magentoApi.post({
+    path: '/index.php/rest/V1/guest-carts'
+  });
+  await magentoApi.post({
+    path: `/index.php/rest/V1/guest-carts/${cartId}/items`,
+    payload: {
+      cartItem: { ...localCartItem, quote_id: cartId }
+    }
+  });
+  await magentoApi.post({
+    path: `/index.php/rest/V1/guest-carts/${cartId}/shipping-information`,
+    payload: {
+      addressInformation: localAddresses
+    }
+  });
+
+  try {
+    const { data: orderId } = await magentoApi.put({
+      path: `/index.php/rest/V1/guest-carts/${cartId}/order`,
+      payload: {
+        paymentMethod: {
+          method: 'checkmo'
+        }
+      }
+    });
+
+    await magentoApi.post({
+      path: `/index.php/rest/V1/order/${orderId}/ship`,
+      payload: {
+        notify: true
+      }
+    });
+
+    return { cartId, orderId };
+  } catch (error) {
+    const util = require('util');
+    console.log(`Error during completing ${cartId}, ${error.message}, ${util.inspect(error.response)}`);
+  }
+};
+
 describe('Deltas Stock Change', function() {
   const sku = '24-MB03';
 
@@ -101,6 +173,23 @@ describe('Deltas Stock Change', function() {
       sinceId: 0
     });
     const product = productDeltas.find(product => product.sku === sku);
+
+    expect(productDeltas.length).to.be.equal(1);
+    expect(product).not.to.be.undefined;
+  });
+
+  it('should return product with stock change from order', async function() {
+    const localCartItem = this.localCartItem;
+
+    await createNewCustomerOrderAndShip(this.magentoApi, localCartItem);
+
+    const { products: productDeltas } = await this.magentoApi.execute('products', 'getDeltas', {
+      page: 1,
+      limit: 3,
+      storeIds: [1, 2],
+      sinceId: 0
+    });
+    const product = productDeltas.find(product => product.sku === localCartItem.sku);
 
     expect(productDeltas.length).to.be.equal(1);
     expect(product).not.to.be.undefined;
