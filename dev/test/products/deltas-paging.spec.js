652be6b3237e9b6ec06ecdd28779da28b259d1b9
Marton Papp <morzzz007@gmail.com>
2020-03-05 10:09:55 +0100

    test(product-deltas): add response test CDPIP-2361
    Co-Authored-By: BlasiusVonSzerencsi <BlasiusVonSzerencsi@users.noreply.github.com>

--- dev/test/products/deltas-paging.spec.js
+++ dev/test/products/deltas-paging.spec.js
@@ -1,18 +1,6 @@
 'use strict';
 
-const insertProductDeltas = async (db, skus) => {
-  const deltas = [];
-
-  for (const sku of skus) {
-    const productEntityRow = await db('catalog_product_entity')
-      .select('entity_id')
-      .where({ sku })
-      .first();
-    deltas.push({ sku, entity_id: productEntityRow.entity_id, row_id: productEntityRow.entity_id });
-  }
-
-  return db('emarsys_product_delta').insert(deltas);
-};
+const { insertProductDeltas } = require('../helpers/product-deltas');
 
 describe('Product Deltas endpoint', function() {
   it('should page through all deltas', async function() {
