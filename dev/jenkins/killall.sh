acd6a070237de195506aa69d9dd030b60108fd01
iben12 <iben@iben.hu>
2020-09-11 11:32:51 +0200

    ops(ci): add networks to killall script CDP-OPS

--- dev/jenkins/killall.sh
+++ dev/jenkins/killall.sh
@@ -4,3 +4,6 @@ set -e
 
 containers=$(docker ps --filter name=mage_* -aq)
 docker rm -f $containers || echo 'No containers found'
+
+networks=$(docker network ls --filter name=mage_* -q)
+docker network rm $networks || echo 'No networks found'
