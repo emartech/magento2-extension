4e2a8cc8b3f7bbd797b9bd432d2446b25edf45ac
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2022-02-10 17:08:01 +0100

    feat(integration-helper): remove Zend URI dependency CDP-1737
    Co-Authored-By: morzzz007 <morzzz007@users.noreply.github.com>
    Co-Authored-By: npospischek <npospischek@users.noreply.github.com>

--- Helper/Integration.php
+++ Helper/Integration.php
@@ -10,8 +10,6 @@ use Magento\Integration\Model\IntegrationService;
 use Magento\Integration\Model\Oauth\Token;
 use Magento\Integration\Model\Oauth\Token\Provider;
 use Magento\Setup\Exception as SetupException;
-use Zend\Uri\Uri;
-use Zend\Uri\UriFactory;
 
 class Integration extends AbstractHelper
 {
@@ -45,7 +43,7 @@ class Integration extends AbstractHelper
      */
     private $context;
 
-    const MAGENTO_VERSION=2;
+    const MAGENTO_VERSION = 2;
 
     /**
      * Integration constructor.
@@ -101,14 +99,9 @@ class Integration extends AbstractHelper
     {
         $token = $this->getToken()['token'];
         $base_url = $this->getBaseUrl();
-        $hostname = $this->getHostFromUrl($base_url);
-        $port = $this->getPortFromUrl($base_url);
-        if ($port && $port !== 80) {
-            $hostname .= ':' . $port;
-        }
         $magento_version = self::MAGENTO_VERSION;
 
-        $connectJson = $this->json->serialize(compact('hostname', 'token', 'magento_version', 'base_url'));
+        $connectJson = $this->json->serialize(compact('token', 'magento_version', 'base_url'));
 
         return base64_encode($connectJson);
     }
@@ -144,24 +137,6 @@ class Integration extends AbstractHelper
         }
 
         return $baseUrl;
-    }
-
-    /**
-     * @param string $url
-     * @return string
-     */
-    private function getHostFromUrl($url)
-    {
-        return UriFactory::factory($url)->getHost();
-    }
-
-    /**
-     * @param string $url
-     * @return string
-     */
-    private function getPortFromUrl($url)
-    {
-        return UriFactory::factory($url)->getPort();
     }
 
     /**
