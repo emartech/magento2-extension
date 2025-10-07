6e9ea787b4e3b87f2daf15dbface425e0c4254d1
Nikolay Dimitrov <dimitrov.nikolay@emarsys.com>
2025-10-07 11:45:58 +0200

    fix: update for code scanners (#75)

--- install-for-codescan.sh
+++ install-for-codescan.sh
@@ -1,2 +1,2 @@
-composer config -g github-oauth.github.com "${GITHUB_TOKEN}"
+composer config -g github-oauth.github.com ${GITHUB_TOKEN}
 composer install --no-autoloader --no-scripts --no-progress --no-suggest --no-interaction --ignore-platform-reqs --no-dev
